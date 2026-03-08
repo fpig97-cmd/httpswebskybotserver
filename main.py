107caef4b0dc411107acd3027f454bc83f65903b
Lunar <fpig97@gmail.com>
2026-03-08 16:13:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+import os
 
 app = FastAPI()
-app.mount("/", StaticFiles(directory="static", html=True), name="static")
+
+# static 폴더 경로 확인
+static_dir = os.path.join(os.path.dirname(__file__), "static")
+
+app.mount("/", StaticFiles(directory=static_dir, html=True), name="static")
